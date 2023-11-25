b16208c24d6ab23afa7e2d0d6970290a171d990e
Ramashish Gupta <ramashishx@gmail.com>
2023-11-26 03:09:12 +0530

    major revamp

---
+++ data/metadata.py
@@ -0,0 +1,106 @@
+[
+    {
+        "ticker":"HDFC",
+        "link": "https://www.moneycontrol.com/news/tags/hdfc.html/news/"
+    },
+    {
+        "ticker":"INFY",
+        "link": "https://www.moneycontrol.com/news/tags/infy.html/news/"
+    },
+    {
+        "ticker":"TCS",
+        "link": "https://www.moneycontrol.com/news/tags/tcs.html/news/"
+    },
+    {
+        "ticker":"HINDUNILVR",
+        "link": "https://www.moneycontrol.com/news/tags/hindustanunilever.html/news/"
+    },
+    {
+        "ticker":"HDFCBANK",
+        "link": "https://www.moneycontrol.com/news/tags/hdfc-bank.html/news/"
+    },
+    {
+        "ticker":"RELIANCE",
+        "link": "https://www.moneycontrol.com/news/tags/reliance.html/news/"
+    },
+    {
+        "ticker":"TATASTEEL",
+        "link": "https://www.moneycontrol.com/news/tags/tatasteel.html/news/"
+    },
+    {
+        "ticker":"ONGC",
+        "link": "https://www.moneycontrol.com/news/tags/ongc.html/news/"
+    },
+    {
+        "ticker":"POWERGRID",
+        "link": "https://www.moneycontrol.com/news/tags/powergrid.html/news/"
+    },
+    {
+        "ticker":"TECHM",
+        "link": "https://www.moneycontrol.com/news/tags/tech-mahindra.html/news/"
+    },
+    {
+        "ticker":"BAJAJ-AUTO",
+        "link": "https://www.moneycontrol.com/news/tags/bajaj-auto.html/news/"
+    },
+    {
+        "ticker":"SUNPHARMA",
+        "link": "https://www.moneycontrol.com/news/tags/sun-pharma.html/news/"
+    },
+    {
+        "ticker":"TITAN",
+        "link": "https://www.moneycontrol.com/news/tags/titan.html/news/"
+    },
+    {
+        "ticker":"BAJAJFINSV",
+        "link": "https://www.moneycontrol.com/news/tags/bajaj-finserv.html/news/"
+    },
+    {
+        "ticker":"LT",
+        "link": "https://www.moneycontrol.com/news/tags/larsen.html/news/"
+    },
+    {
+        "ticker":"ASIANPAINT",
+        "link": "https://www.moneycontrol.com/news/tags/asian-paints.html/news/"
+    },
+    {
+        "ticker":"AXISBANK",
+        "link": "https://www.moneycontrol.com/news/tags/axis-bank.html/news/"
+    },
+    {
+        "ticker":"HCLTECH",
+        "link": "https://www.moneycontrol.com/news/tags/hcl-tech.html/news/"
+    },
+    {
+        "ticker":"SBIN",
+        "link": "https://www.moneycontrol.com/news/tags/sbi.html/news/"
+    },
+    {
+        "ticker":"MARUTI",
+        "link": "https://www.moneycontrol.com/news/tags/maruti.html/news/"
+    },
+    {
+        "ticker":"NESTLEIND",
+        "link": "https://www.moneycontrol.com/news/tags/nestle.html/news/"
+    },
+    {
+        "ticker":"M&M",
+        "link": "https://www.moneycontrol.com/news/tags/mahindra-and-mahindra.html/news/"
+    },
+    {
+        "ticker":"ULTRACEMCO",
+        "link": "https://www.moneycontrol.com/news/tags/ultratech-cement.html/news/"
+    },
+    {
+        "ticker":"KOTAKBANK",
+        "link": "https://www.moneycontrol.com/news/tags/kotak-mahindra-bank.html/news/"
+    },
+    {
+        "ticker":"ITC",
+        "link": "https://www.moneycontrol.com/news/tags/itc.html/news/"
+    },
+    {
+        "ticker":"BHARTIARTL",
+        "link": "https://www.moneycontrol.com/news/tags/airtel.html/news/"
+    }
+]
